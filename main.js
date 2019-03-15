5005eb6ffb4784311bbc90dc4b2badd5559d50d3
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-15 23:30:50 +0100

    eventListener

--- main.js
+++ main.js
@@ -105,7 +105,8 @@ var UIController = (function() {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   };
 
   return {
@@ -183,6 +184,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
   };
 
   var updateBudget = function() {
